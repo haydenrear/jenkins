767372785c525cb9ff4fdcd325601130982d6ef6
hayden.rear <hayden.rear@gmail.com>
2025-02-15 18:15:44 -0500

    updated

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -1,4 +1,7 @@
 cd ..
 
+#openssl s_client -showcerts -connect localhost:5001 < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ca.crt
+#cp ca.crt crts/ca.crt
+
 git daemon --export-all --reuseaddr --base-path=/Users/hayde/IdeaProjects -- /Users/hayde/IdeaProjects/drools &
 git daemon --export-all --reuseaddr --base-path=/Users/hayde/IdeaProjects -- /Users/hayde/IdeaProjects/versions-parent --port=9419 &
